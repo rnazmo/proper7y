e88af0b98af39ee5cd99ebfe54a4ecf3ea445826
rnazmo <rnazmo@gmail.com>
2021-06-29 02:53:31 +0900

    Bump a version to v0.1.6

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ set -eu
 # If you want to download this script from remote and run it with one liner, run like:
 #   $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/rnazmo/property/main/install.sh ${HOME}/bin/)"
 
-PROPERTY_VERSION="v0.1.5"
+PROPERTY_VERSION="v0.1.6"
 
 SRC_URL="https://raw.githubusercontent.com/rnazmo/property/${PROPERTY_VERSION}/property"
 
