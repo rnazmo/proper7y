0b73db32eca421948c2bcc4ddffe6c9152933f52
rnazmo <rnazmo@gmail.com>
2024-09-29 15:27:53 +0900

    feat: Change shebang `#!/bin/bash` -> `#!/usr/bin/env bash`
    To use Bash installed with brew on macOS.

--- devel-tools/script/install-devel-tools.linux-x64.sh
+++ devel-tools/script/install-devel-tools.linux-x64.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 # TL:DR (What is this?)
@@ -7,20 +7,20 @@ set -eu
 source "$(dirname "$0")/common.sh"
 
 main() {
-  log_info "Start installing..."
+    log_info "Start installing..."
 
-  # 1. Check if the DEVEL_TOOLS_BIN_DIR exists
-  check_if_devel_tools_bin_dir_exists
+    # 1. Check if the DEVEL_TOOLS_BIN_DIR exists
+    check_if_devel_tools_bin_dir_exists
 
-  # 2. Install shellcheck
-  install_shellcheck
-  check_shellcheck_is_ready
+    # 2. Install shellcheck
+    install_shellcheck
+    check_shellcheck_is_ready
 
-  # 3. Install shfmt
-  install_shfmt
-  check_shfmt_is_ready
+    # 3. Install shfmt
+    install_shfmt
+    check_shfmt_is_ready
 
-  log_info "Installed successflly!"
+    log_info "Installed successflly!"
 }
 
 main
--- devel-tools/script/run-integ-test.linux-x64.sh
+++ devel-tools/script/run-integ-test.linux-x64.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 # TL;DR (What is this?):
