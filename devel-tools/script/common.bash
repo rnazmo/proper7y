57cdd8b93cce3740caae7691064e377ca0a812ab
rnazmo <rnazmo@gmail.com>
2026-04-27 20:34:27 +0900

    chore: Bump project version: v0.9.9 -> v0.9.10

--- devel-tools/script/common.bash
+++ devel-tools/script/common.bash
@@ -64,7 +64,7 @@ set -euo pipefail
 # ============================================================
 # Class A: True constants (never change)
 # ============================================================
-readonly PROPER7Y_VERSION="v0.9.9"
+readonly PROPER7Y_VERSION="v0.9.10"
 readonly SHELLCHECK_TOOL_NAME="shellcheck"
 readonly SHFMT_TOOL_NAME="shfmt"
 
